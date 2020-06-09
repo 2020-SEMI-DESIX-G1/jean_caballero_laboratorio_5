224d237b926fb4274d6750a6e48c1f32b07a8c55
jckballero <jean.caballero29@hotmail.com>
2020-06-09 22:20:27 +0000

    Labortorio terminado

---
+++ Laboratorio5.js
@@ -0,0 +1,35 @@
+/*Laboratorio 5
+ Jean Carlos Caballero*/
+
+//Primera parte
+var a = 10;
+var b = 5;
+
+console.log(`Suma: ${a} + ${b} = ${a + b}`);
+console.log(`Resta: ${a} - ${b} = ${a - b}`);
+console.log(`Multiplicación: ${a} * ${b} = ${a * b}`);
+console.log(`División: ${a} / ${b} = ${a / b}`);
+
+//Segunda parte
+let texto1 = "Uniendo";
+let texto2 = "Textos";
+
+console.log(texto1.concat(" "+texto2));
+
+//Tercera parte
+const num1 = 29;
+const num2 = "Lab5";
+
+console.log(typeof(num1));
+console.log(typeof(num2));
+
+//Cuarta parte
+const obj = {
+	num: 29,
+	text: "Laboratorio 5",
+	salida: true,
+	vacio:[]
+}
+
+console.log(obj);
+
